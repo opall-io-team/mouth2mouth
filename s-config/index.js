b78aef812a3d5d41642542b7ac5895b798508681
aleem-ahmed <opall.io2020@gmail.com>
2021-01-17 17:37:45 -0500

    Preparing for Deployment

---
+++ s-config/index.js
@@ -0,0 +1,22 @@
+// [REQUIRE] //
+require('dotenv').config()
+
+
+// [EXPORT] //
+module.exports = {
+	// [URL + PORT] //
+	BASE_URL: process.env.BASE_URL || 'http://localhost:8080',
+	SOCKET_BASE_URL: process.env.BASE_URL || 'http://localhost:5000',
+	PORT: process.env.PORT || 5000,
+	
+	// [MONGODB] //
+	MONG_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/m2m',
+	
+	// [SECRET] //
+	SECRET_KEY: process.env.SECRET_KEY || 'secret',
+
+
+	// [IEX] //
+	IEX_PUBLIC_KEY: process.env.IEX_PUBLIC_KEY || '',
+	IEX_SB_PUBLIC_KEY: process.env.IEX_SB_PUBLIC_KEY || '',
+}
--- server.js
+++ server.js
@@ -1,27 +1,55 @@
 // [REQUIRE] //
 const cors = require('cors')
 const express = require('express')
+const http = require('http')
+const mongoose = require('mongoose')
 
 
 // [REQUIRE] Personal // 
+const config = require('./s-config')
 const p_ = require('./s-routes/pages')
 const p_about = require('./s-routes/pages/about')
 
 
-
 // [EXPRESS] //
 const app = express()
+const server = http.createServer(app)
 
 
-// [INIT] Const //
-const port = 5000
+// [MONGOOSE-CONNECTION] //
+mongoose.connect(
+	config.MONG_URI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	},
+	(err, connected) => {
+		if (connected) { console.log('Mongoose Connected to DB') }
+		else { console.log(`Mongoose Connection Error --> ${err}`) }
+	}
+)
 
+
+// [USE] //
 app.use(cors())
 
+
+// [USE] Personal - Rate-Limiter / API / Pages //
 app.use('/pages', p_)
 app.use('/pages/about', p_about)
 
 
-app.listen(port, () => {
-  console.log(`server started on port: ${port}`)
-})
+// [HEROKU] Set Static Folder for Heroku //
+if (process.env.NODE_ENV == 'production') {
+	app.use(express.static('client/dist'))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+	})
+}
+
+
+// [PORT + LISTEN] //
+const port = config.PORT
+server.listen(port, () => { console.log(`Server Running on Port: ${port}`) })
