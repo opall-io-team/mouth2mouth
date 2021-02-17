3963715ce20e4e76e7bd587ed429da5d0f2b4292
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-02-17 04:19:17 -0500

    Improved

--- client/src/pages/gallery.vue
+++ client/src/pages/gallery.vue
@@ -1,30 +1,31 @@
 <template>
 	<BContainer class="my-4">
 		<BCard bg-variant="light" class="mt-3 shadow">
-			<h1 class="mb-3 text-center font-weight-bold text-primary">{{ pageData.title }}</h1>
+			<!-- Page Title -->
+			<h1 class="mb-3 text-center font-weight-bold text-primary">
+				{{ pageData.title }}
+			</h1>
 
-			<!-- Eyebrows -->
 			<viewer :options="{ title: false, transition: false, }">
-				<!-- [Rx] -->
+				<!-- [Rx] Image Section -->
 				<BRow v-for="(row, index) in pageData.rx" :key="index" class="mb-3">
+					<!-- [C1] Title -->
 					<BCol cols="12">
-						<p class="h2 text-info">{{ row.c1.title }}</p>
+						<p class="mb-3 h2 text-info">{{ row.c1.title }}</p>
 					</BCol>
 
-					<!-- [CX] -->
+					<!-- [CX] Images -->
 					<BCol
 						v-for="(image, index) in row.cx"
 						:key="index"
 						cols="12" sm="6" md="4" lg="4" xl="3"
 					>
-						<BCard variant="white" border-variant="white" class="mb-3 shadow rounded-0">
-							<img
-								:src="image"
-								alt="No Photo"
-								class="w-100"
-								style="height: 300px; object-fit: cover;"
-							>
-						</BCard>
+						<img
+							:src="image"
+							alt="No Photo"
+							class="w-100 mb-4 shadow"
+							style="height: 300px; object-fit: cover;"
+						>
 					</BCol>
 				</BRow>
 			</viewer>
