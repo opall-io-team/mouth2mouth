98d095cc7ef6f062213ce9c753581d058ba7adf2
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-02-24 13:29:17 -0500

    stripe dev

--- client/src/pages/payment/index.vue
+++ client/src/pages/payment/index.vue
@@ -2,75 +2,66 @@
 	<BContainer class="my-3">
 		<BRow>
 			<BCol cols="12">
-				<BCard bg-variant="white">
-					<h1>Payment</h1>
+				<BCard variant="white">
+					<h1>Please give us your payment details:</h1>
 
-					<StripeCheckout
-						ref="checkoutRef"
-						mode="payment"
-						:line-items="lineItems"
-						:success-url="`${successURL}/${sessionId}`"
-						:cancel-url="cancelURL"
-						:pk="publicKey"
-						:session-id="sessionId"
+					<Card
+						stripe='pk_test_51INvnfCC0rHo3XXZIRZAaLkXUGf7iVmAeycF7sA0FrsBRfK5TgrZHyYxBndMY0x7mrMEyUW2Xp9TdgVYC8gGkgxr00X5ZcfZM6'
+						class='mb-3 stripe-card'
+						:class='{ complete }'
+						@change="complete = $event.complete"
 					/>
-					
+
 					<BButton
-						:disabled="loading"
+						:disabled='!complete'
 						variant="primary"
-						@click="submit()"
-					>Pay Deposit Now</BButton>
+						class="pay-with-stripe"
+						@click="pay"
+					>Pay with credit card</BButton>
+
+					<h1 class="text-danger">{{ error }}</h1>
 				</BCard>
 			</BCol>
 		</BRow>
 	</BContainer>
 </template>
-
-
+ 
 <script>
-	import { StripeCheckout } from '@vue-stripe/vue-stripe'
+import { Card, createToken } from 'vue-stripe-elements-plus'
+ 
+export default {
+	components: { Card },
+		
+	data () {
+		return {
+			complete: false,
+			error: ''
+		}
+	},
+ 
+	methods: {
+		pay () {
+			// createToken returns a Promise which resolves in a result object with
+			// either a token or an error key.
+			// See https://stripe.com/docs/api#tokens for the token object.
+			// See https://stripe.com/docs/api#errors for the error object.
+			// More general https://stripe.com/docs/stripe.js#stripe-create-token.
 
-	export default {
-		components: {
-			StripeCheckout,
-		},
+			createToken()
+				.then(data => console.log(data.token))
+				.catch(err => this.error = err)
 
-		data () {
-			return {
-				loading: false,
-				sessionId: 'testId',
-				publicKey: 'pk_test_51INvnfCC0rHo3XXZIRZAaLkXUGf7iVmAeycF7sA0FrsBRfK5TgrZHyYxBndMY0x7mrMEyUW2Xp9TdgVYC8gGkgxr00X5ZcfZM6',
-				lineItems: [
-					{
-						price: 'price_1IO7UYCC0rHo3XXZenRbrkpv',
-						quantity: 1,
-					},
-				],
-				successURL: '',
-				cancelURL: '',
-			}
-		},
-
-		created() {
-			this.determinUrls()
-		},
-
-		methods: {
-			determinUrls() {
-				if (window.webpackHotUpdate) {
-					this.successURL = 'http://localhost:8080/payment/'
-					this.cancelURL = 'http://localhost:8080/payment/'
-				}
-				else {
-					this.successURL = 'http://localhost:8080/'
-					this.cancelURL = 'http://localhost:8080/'
-				}
-			},
-
-			submit() {
-			// You will be redirected to Stripe's secure checkout page
-			this.$refs.checkoutRef.redirectToCheckout()
-			},
-		},
+		}
 	}
-</script>
+}
+</script> 
+ 
+<style>
+	.stripe-card {
+		width: 300px;
+		border: 1px solid grey;
+	}
+	.stripe-card.complete {
+		border-color: green;
+	}
+</style> 
