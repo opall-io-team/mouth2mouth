46e1446788251f5d3481773bfa30d2138385da23
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-02 22:05:56 -0400

    Improved

--- client/src/defaults/pages/services.js
+++ client/src/defaults/pages/services.js
@@ -15,17 +15,17 @@ module.exports = {
 		{
 			id: 'chakra-balancing',
 			title: 'Chakra Balancing',
-			description: ''
+			description: 'Chakra balancing is the process of restoring a harmonious flow of energy across the chakra system. The effect of well-balanced chakras often translates into a feeling of balanced well-being, relaxation, centeredness, increased vitality, and embodiment of oneself.  Most of us have energetic blocks and imbalances as well as energy-sabotaging habits that prevent us from accessing our full vitality, which leads us to feel exhausted, scattered, dull… even ill. Chakra Clearing and Balancing are necessary to balance the other operating systems in one\'s body.'
 		},
 		{
 			id: 'crystal-healing',
 			title: 'Crystal Healing',
-			description: ''
+			description: 'Crystal healing is an alternative medical technique in which crystals and other stones are used to cure ailments and protect against disease. Proponents of this technique believe that crystals act as conduits for healing — allowing positive, healing energy to flow into the body as negative, disease-causing energy flows out.  During a treatment session, I may place various crystals on your body along with the chakra points or roughly in the regions. The stones are positioned in chosen areas for the reported by the patient or what may show through my intuition.'
 		},
 		{
 			id: 'foot-detox-and-spa',
 			title: 'Foot Detox and Spa',
-			description: ''
+			description: 'Toxins can accumulate in the body from a wide range of sources, such as parasites, heavy metals, and other toxic substances. These toxins can contribute to disorders that have vibrational frequencies that interfere with the natural polarity and oscillation of the healthy cells in the body. This disruption can cause an imbalance in the healthy cells and interfere with the natural healing power of the body. Some of the symptoms that you may have toxins in the body include digestive problems, headaches, constipation, bad breath, poor circulation, weight gain or obesity, acne, allergies, cellulite, and forgetfulness.',
 		},
 		{
 			id: 'oracle-card-readings',
