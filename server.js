e6ead7e6e6d30319f177ee2201513db17357991d
aleem ahmed <61424530+aleem-ahmed@users.noreply.github.com>
2021-09-13 13:24:29 -0400

    remove weird space

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
-
 const http = require('http')
 const mongoose = require('mongoose')
 const path = require('path')
