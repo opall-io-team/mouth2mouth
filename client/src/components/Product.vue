a655881272e7bd2b9f1d7f8dca4d6f5386a08ca7
aleem-ahmed <opall.io2020@gmail.com>
2021-09-22 19:42:45 -0400

    Improved

--- client/src/components/Product.vue
+++ client/src/components/Product.vue
@@ -16,6 +16,7 @@
 
 		<BCol cols="12" md="9">
 			<p>{{ product.description }}</p>
+			<hr>
 
 			<!-- [CONTENT] -->
 			<div v-for="(c, i) in product.content" :key="i">
@@ -33,9 +34,10 @@
 							class="m-0 py-1"
 						>{{ item }}</BListGroupItem>
 					</BListGroup>
+					<hr>
 				</div>
 
-				<!-- [LIST] -->
+				<!-- [CHECK-LIST] -->
 				<div v-if="c.type == 'check-list'" class="mb-3">
 					<BListGroup class="border-0 transparent">
 						<BListGroupItem class="px-0 border-0 transparent">
@@ -49,37 +51,48 @@
 							class="m-0 py-1 border-0 transparent"
 						>✓{{ item }}</BListGroupItem>
 					</BListGroup>
+					<hr>
 				</div>
 
 				<!-- [PARAGRAPH] -->
 				<div v-if="c.type == 'paragraph'" class="mb-3">
 					<h5 class="text-primary">{{ c.title }}</h5>
 					<p>{{ c.content }}</p>
+					<hr>
 				</div>
 			</div>
-			<hr>
 
 			<!-- [PRICING] -->
-			<div class="">
-				<BListGroup>
-					<BListGroupItem variant="light" class="text-center">
-						<h5 class="m-0 text-primary">Pricing</h5>
+			<div v-if="product.pricing.length > 0" class="">
+				<BListGroup class="mx-auto border" style="max-width: 400px;">
+					<BListGroupItem
+						variant="primary"
+						class="
+							text-center
+							border-light
+							border-bottom
+							border-top-0
+							border-left-0
+							border-right-0
+						"
+					>
+						<h5 class="m-0 text-primary font-weight-bold">Pricing</h5>
 					</BListGroupItem>
 		
 					<BListGroupItem
 						v-for="(p, i) in product.pricing"
 						:key="i"
-						variant="light"
-						class="py-2"
+						variant="primary"
+						class="py-2 border-0"
 					>
 						<BRow>
 							<BCol cols="12" md="9">
-								<h6 class="m-0 text-primary">
+								<h6 class="m-0 text-dark">
 									{{ p.title }}
 								</h6>
 							</BCol>
 							<BCol cols="12" md="3">
-								<h6 class="m-0 text-success">
+								<h6 class="m-0 text-dark">
 									{{ p.costString }}
 								</h6>
 							</BCol>
