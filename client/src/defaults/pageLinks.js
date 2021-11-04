14c37979fece1a9a2e74918bf81ccf212f15b862
aleem ahmed <61424530+aleem-ahmed@users.noreply.github.com>
2021-11-04 11:30:06 -0400

    ss

--- client/src/defaults/pageLinks.js
+++ client/src/defaults/pageLinks.js
@@ -4,28 +4,14 @@
 
 // [EXPORT] //
 module.exports = [
-	/*
 	{
 		path: '/',
-		text: 'Home',
-		navIcon: feather.icons.home.toSvg({
-			'stroke-width': 2.4,
-			width: 16,
-			height: 16,
-			'class': ''
-		}),
-		slideMenuIcon: feather.icons.home.toSvg({
-			'stroke-width': 2.4,
-			width: 34,
-			height: 34,
-			'class': 'my-2'
-		}),
+		text: 'Welcome',
 		children: [],
 	},
-	*/
 	{
 		path: '/services',
-		text: 'Services',
+		text: 'Therapies',
 		children: [
 			{
 				path: '/services/reiki',
@@ -57,12 +43,14 @@ module.exports = [
 		text: 'Contact',
 		children: [],
 	},
+	/*
 	{
 		path: '/gallery',
 		text: 'Gallery',
 		children: [],
-
+		
 	},
+	*/
 	{
 		path: '/faq',
 		text: 'FAQ',
