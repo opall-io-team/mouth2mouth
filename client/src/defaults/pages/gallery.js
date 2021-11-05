deb302ec5b28b44b6e6c0fbb0fe376ff88e9fffc
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-11-05 10:45:58 -0400

    Improved

--- client/src/defaults/pages/gallery.js
+++ client/src/defaults/pages/gallery.js
@@ -13,19 +13,7 @@ module.exports = {
 		{
 			c1: { title: 'Other' },
 	
-			cx: [
-				require('../../assets/images/pages/gallery/other/0.jpg'),
-				require('../../assets/images/pages/gallery/other/1.jpg'),
-				require('../../assets/images/pages/gallery/other/2.jpg'),
-				require('../../assets/images/pages/gallery/other/3.jpg'),
-				require('../../assets/images/pages/gallery/other/4.jpg'),
-				require('../../assets/images/pages/gallery/other/5.jpg'),
-				require('../../assets/images/pages/gallery/other/6.jpg'),
-				require('../../assets/images/pages/gallery/other/7.jpg'),
-				require('../../assets/images/pages/gallery/other/8.jpg'),
-				require('../../assets/images/pages/gallery/other/9.jpg'),
-				require('../../assets/images/pages/gallery/other/10.jpg'),
-			]
+			cx: []
 		},
 	]
 }
