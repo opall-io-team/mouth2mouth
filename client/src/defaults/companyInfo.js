5c41fa8ba83a6c647f22b503473619259dce2b39
aleem-ahmed <opall.io2020@gmail.com>
2021-11-16 15:41:35 -0500

    Improved

--- client/src/defaults/companyInfo.js
+++ client/src/defaults/companyInfo.js
@@ -5,7 +5,7 @@ module.exports = {
 	companyCaption1: 'Embrace wellness with Pathways to Inner Balance',
 	companyCaption2: '',
 
-	companyLogo: require('../assets/logov4.png'),
+	companyLogo: require('../assets/logov5.png'),
 
 	address: '',
 	googleMapsLink: '',
